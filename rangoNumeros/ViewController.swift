e244445e59160dddbc71e6087a913bf06d08c601
josman185hotm <josman185@gmail.com>
2016-05-31 00:26:29 -0400

    x

--- rangoNumeros/ViewController.swift
+++ rangoNumeros/ViewController.swift
@@ -13,6 +13,26 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        for numero in 0...100 {
+            
+            let divCinco = numero % 5
+            if divCinco == 0 {
+                print("\(numero) Bingo!!!" )
+            }
+            
+            let esPar = numero % 2
+            if esPar == 0 {
+                print("\(numero) par!!!")
+            } else if esPar != 0 {
+                print("\(numero) impar!!!")
+            }
+            
+            if numero >= 30 && numero <= 40 {
+                print("\(numero) VIVA Swift!!!")
+            }
+            
+        }
+
     }
 
     override func didReceiveMemoryWarning() {
