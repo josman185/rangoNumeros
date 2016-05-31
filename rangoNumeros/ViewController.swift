56d2e5d752be15f5796da85cc1b1cdc7e2a9a703
josman185hotm <josman185@gmail.com>
2016-05-31 00:18:20 -0400

    Initial Commit

---
+++ rangoNumeros/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  rangoNumeros
+//
+//  Created by MCS on 5/31/16.
+//  Copyright © 2016 MCS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
